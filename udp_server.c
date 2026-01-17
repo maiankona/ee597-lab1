0431ca071eaf7b2b4584a841c806a626c8abb340
Maia Nkonabang <nkonaban@usc.edu>
2026-01-17 15:53:32 -0800

    Update udp_server.c

--- udp_server.c
+++ udp_server.c
@@ -4,20 +4,7 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <signal.h>
 
-void dostuff(int *newsockfd)
-{
-int n;
-char buffer[256];
-bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-
-}
 void error(char *msg)
 {
     perror(msg);
@@ -34,7 +21,7 @@ int main(int argc, char *argv[])
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -45,31 +32,9 @@ int main(int argc, char *argv[])
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
-     //listen(sockfd,5);
-    fromlen = sizeof(struct sockaddr_in);
-
-     //clilen = sizeof(cli_addr);
-signal(SIGCHLD,SIG_IGN);
-
-/*while (1)
-{
-int pid;
-//newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-if (newsockfd < 0)
-error("ERROR on accept");
-pid = fork();
-if (pid < 0)
-error("ERROR on fork");
-if (pid == 0)
-{
-close(sockfd);
-dostuff(newsockfd);
-exit(0);
-}
-else
-close(newsockfd);
-} /* end of while */
-/*     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     /*listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
      if (newsockfd < 0) 
           error("ERROR on accept");
      bzero(buffer,256);
@@ -78,10 +43,15 @@ close(newsockfd);
      printf("Here is the message: %s\n",buffer);
      n = write(newsockfd,"I got your message",18);
      if (n < 0) error("ERROR writing to socket");*/
+      fromlen = sizeof(struct sockaddr_in);
       while (1)
-    {
-      n = recvfrom(newsockfd,buffer,255,0,(struct sockaddr_in *)&from,&fromlen);
-      if (n < 0) error("recvfrom");
-    }
+      {
+         n = recvfrom(newsockfd,buffer,255,0,(struct sockaddr *)&from,&fromlen);
+         if (n < 0) error("recvfrom");
+         printf("Here is the message: %s\n",buffer); //from original
+         n = sendto(newsockfd,"I got your message",18,0,(struct sockaddr *) &from,fromlen);
+         if (n < 0)
+         error("sendto");
+      }
      return 0; 
 }
