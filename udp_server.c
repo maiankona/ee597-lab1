e0f96f5a509d8a0e73c47c02831d5150c809dad2
Maia Nkonabang <nkonaban@usc.edu>
2026-01-17 16:12:31 -0800

    Update udp_server.c

--- udp_server.c
+++ udp_server.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno, clilen;
      char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
+     struct sockaddr_in serv_addr, cli_addr, from;
      int n;
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
@@ -43,13 +43,14 @@ int main(int argc, char *argv[])
      printf("Here is the message: %s\n",buffer);
      n = write(newsockfd,"I got your message",18);
      if (n < 0) error("ERROR writing to socket");*/
-      int fromlen = sizeof(struct sockaddr_in);
+      socklen_t fromlen = sizeof(struct sockaddr_in);
       while (1)
       {
-         n = recvfrom(newsockfd,buffer,255,0,(struct sockaddr_in *)&from,&fromlen);
+         bzero(buffer,256);
+         n = recvfrom(sockfd,buffer,255,0,(struct sockaddr *)&from,&fromlen);
          if (n < 0) error("recvfrom");
          printf("Here is the message: %s\n",buffer); //from original
-         n = sendto(newsockfd,"I got your message",18,0,(struct sockaddr_in *) &from,fromlen);
+         n = sendto(sockfd,"I got your message",18,0,(struct sockaddr *) &from,fromlen);
          if (n < 0)
          error("sendto");
       }
